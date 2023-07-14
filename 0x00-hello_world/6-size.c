020135827225f5df21b066374f95fd034d757c10
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-13 20:04:36 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriotion: A C program that prints with put function
+ *
+ * Return: Alaws 0 (success)
+*/
+
+int main(void)
+{
+
+printf("Size of a char: %u byte(s)\n", sizeof(char));
+printf("Size of a int: %u byte(s)\n", sizeof(int));
+printf("Size of a long int: %u byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
+printf("Size of a float: %u byte(s)\n", sizeof(float));
+return (0);
+
+}
