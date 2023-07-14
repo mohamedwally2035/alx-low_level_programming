95cf3767727eac75d090d991a7b8273903676404
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-14 16:25:44 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Descriotion: print all alphabet letters
+ *
+ * Return: Alaws 0 (success)
+*/
+
+int main(void)
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+putchr(ch);
+ch++;
+
+}
+putchar('\n');
+
+return (0);
+}
