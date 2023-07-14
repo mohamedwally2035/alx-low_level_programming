c86df70909e3e0ea3845110b02bda47e69596fd2
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-13 17:57:54 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+
+#include <stdio>
+
+/**
+ * main - Entry point
+ *
+ * Descriotion: A C program that prints with put function
+ *
+ * Return: Alaws 0 (success)
+*/
+
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}	
