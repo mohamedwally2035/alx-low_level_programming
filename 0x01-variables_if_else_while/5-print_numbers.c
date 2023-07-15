ffcfbb3e831a1081f045dab92751f6baa54b658f
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-14 17:04:17 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descriotion:  Prints numbers from 0 to 9.
+ *
+ * Return: Alaws 0 (success)
+*/
+init main(void)
+
+{
+
+int i;
+
+for (i = 0; i < 10; i++)
+
+{
+
+printf("%d", i);
+
+}
+
+putchar('\n');
+
+return (0);
+
+}
+
