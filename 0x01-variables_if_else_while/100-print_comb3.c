d5cd6110956889bc4fc294a43ac1662ef68d9003
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-16 13:03:12 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-
 /**
  * main - Entry point
  *
@@ -13,29 +12,31 @@ int main(void)
 
 {
 
-int n, m, l;
+int n, m;
 
 
-for (n = 48; n < 58; n++)
-
-{
-for (m = 49; m < 58; m++)
+for (n = 48; n <= 56; n++)
 
 {
 
-{
-if (l > m && m > n)
+for (m = 49; m <= 57; m++)
+
 {
 
+if (m > n)
+
+{
 putchar(n);
 putchar(m);
 
-if (n != 55 || m != 56)
+if (n != 56 || m != 57)
+
 {
-putchar(',');
+
 putchar(' ');
+
 }
-}
+
 }
 }
 }
