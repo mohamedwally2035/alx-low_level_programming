14a4d6d82337b907a0f313b05bbfa03c8e4ceaa3
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-18 15:11:42 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,24 @@
  * Return: Alaws 0 (success)
 */
 
+
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
 
 	for (ch = 0; ch < 8; ch++)
-			_putchar(str[ch]);
-				_putchar('\n');
+		_putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
+
+
+
+
+
+
+
+
 
 }
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -11,9 +11,9 @@
 */
 
 int _putchar(char c)
-
 {
-	return (write(1,&c, 1));
+	return (write(1, &c, 1));
+
 
 
 
