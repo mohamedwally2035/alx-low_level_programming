13c8d57e39bc65041c3b795b110a2df80bd72272
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-18 14:58:59 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,12 +7,13 @@
  * @c: The character to print
  *
  * Return: on success 1.
- *         on error, -1 is returned, and errno is set appropi ately.
+ * on error, -1 is returned, and errno is set appropi ately.
 */
 
 int _putchar(char c)
+
 {
-	return (write(1, &c, 1));
+	return (write(1,&c, 1));
 
 
 
