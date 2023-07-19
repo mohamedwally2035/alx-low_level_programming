435c14b3ee6e24130e0bf0884577daf12f8300eb
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-18 18:20:34 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 
 			prod = num * mult;
 
-			/**
+			/*
 			 * put space if product is a single number
 			 * place the first digit if its two numbers
 			 */
@@ -37,7 +37,5 @@ void times_table(void)
 
 		}
 		_putchar('\n');
-	}
-		}
 	}
 }
