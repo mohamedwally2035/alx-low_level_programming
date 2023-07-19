22b217e57cd3872cf5c6e9442798b16a8b0bc1be
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-18 17:37:28 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,10 +13,10 @@ void jack_bauer(void)
 		for (min = 0; min <= 59; min++)
 		{
 			_putchar((hr / 10) + 48);
-			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
 			_putchar(':');
 			_putchar((min / 10) + 48);
-			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
 			_putchar('\n');
 		}
 	}
