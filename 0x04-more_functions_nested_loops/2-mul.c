a100d38dcec2a639db138ba5268be6ff9dfb97a6
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-20 09:49:36 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiolies two integers
+ *
+ * @a: furst input
+ * @b: second input
+ *
+ * Return: return resilts
+*/
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
