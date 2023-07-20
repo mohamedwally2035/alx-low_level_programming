c16c81faf9d623c75881e0ae0c102d4cf55e5893
mohamedwally2035 <mohamedwally87@gmail.com>
2023-07-20 09:09:40 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "main.h"
+
+
+/**
+ * _putchar - writes the character c to srdout
+ * @c: The character to print
+ *
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropi ately.
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+
+
+
+}
