211f9568b08fd9b7b72746c8ffa1332fef32784c
mohamedwally2035 <mohamedwally87@gmail.com>
2023-08-06 15:02:55 -0700

    libmy

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - function to check if charcter is function
+ *
+ * @c: checks input of function
+ *
+ * Return: returns 1 if `c` is lowercase
+ * otherwise always 0 (success)
+*/
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
