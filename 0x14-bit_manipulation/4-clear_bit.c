71d1fdbbb08c1b3f050a2b6fc4c5ba549801a545
mohamedwally2035 <mohamedwally87@gmail.com>
2024-02-20 12:18:57 -0800

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,7 +10,7 @@ int clear_bit(unsigned long int *n unsigned int index)
 {
 	if (index >= sizeof(n) * 8)
 		return (-1);
-	if (*n & 1L << index)
-		*n ^= 1L << index;
+	if (*n & 1l << index)
+		*n ^= 1l << index;
 	return (1);
 }
