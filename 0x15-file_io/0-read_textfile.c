a14e196f66f4f4de3733f218e8256c1f9381a21c
mohamedwally2035 <mohamedwally87@gmail.com>
2024-02-21 05:01:20 -0800

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
@@ -10,7 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t nrd, nwr;
-	char buf[READ_BUF_SIZE *8];
+	char *buf;
 
 	if (!filename)
 		return (0);
